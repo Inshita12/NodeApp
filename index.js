df1569643749259219403b0d3a50b4c7367f5987
Inshita Bamba <inshitabamba@gmail.com>
2021-06-09 01:43:17 +0530

    abcde

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
 const http = require('http');
-
 const port = 8000;
 const fs = require('fs');
+var path = require('path');
 
-function requestHandler(request, response) {
-  console.log(request.url);
-
-  response.writeHead(200, { 'content-type': 'text/html' }); //write Head is function but we can write anyname in place contentt-type
-
-  // response.end('<h1>Hey This is my first server</h1>')
-
-  let filePath;
-
-  switch(request.url){
-      case'/':
-        filePath='./index.html'
-        break;
-      case'/profile':
-        filePath='./profile.html'  
-        break;
-      default:
-          filePath='./404.html'  
-  }
-  fs.readFile  
-    (filePath,
-    function (error, data) {
-      if (error) {
-        console.log('Error:', error);
-        return response.end('<h1>Oops! There is some Error</h1>');
-      }
-      return response.end(data);
+function requestHandler(req, res) {
+    console.log('url', req.url);
+    let filepath = '.' + req.url;
+    let contentType = 'text/html';
+    switch (req.url) {
+        case '/':
+            filepath = './index.html';
+            break;
+        case '/profile':
+            filepath = './profile.html';
+            break;
+        case '/resume':
+            filepath = './Resume/index.html';
+            break;
+        default:
+    }
+    let extname = path.extname(filepath);
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+        case '.ico':
+            filepath = './404.html';
+            contentType = 'text/html';
+            break;
+        case '':
+            filepath = './404.html';
+            contentType = 'text/html';
+            break;
+    }
+    fs.readFile(filepath, function (err, data) {
+        if (err) {
+            console.log('error', err);
+            return res.end('<h1>Error</h1>');
+        }
+        res.writeHead(200, { 'content-type': contentType });
+        return res.end(data);
     });
 }
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, function (err) {
-  if (err) {
-    console.log('Error');
-    return;
-  } else {
-    console.log('Server is running on port:', port);
-  }
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Server is up and running on port', port);
 });
